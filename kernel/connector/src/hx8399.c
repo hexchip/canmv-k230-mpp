4d7f306cec5da249e2870bbe407980d9db3d3e1d
xiaenling <xiaenling@canaan-creative.com>
2025-03-07 09:40:08 +0800

    Update panel hx8399.

--- kernel/connector/src/hx8399.c
+++ kernel/connector/src/hx8399.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023, Canaan Bright Sight Co., Ltd
+/* Copyright (c) 2025, Canaan Bright Sight Co., Ltd
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -21,20 +21,13 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+*/
 
 #include "connector_dev.h"
 #include "io.h"
 #include "drv_gpio.h"
 #include "k_vo_comm.h"
 #include "k_connector_comm.h"
-
-#define DELAY_MS_BACKLIGHT_DEFAULT     200
-#define DELAY_MS_BACKLIGHT_FIRST       1
-
-static k_u8 hx8399_y_mirror = 0;
-
-static k_s32 g_blacklight_delay_ms = DELAY_MS_BACKLIGHT_FIRST;
 
 static void hx8399_v2_init(k_u8 test_mode_en)
 {
@@ -56,7 +49,7 @@ static void hx8399_v2_init(k_u8 test_mode_en)
         0x39, 0,    9,  0xD8, 0xE2, 0xAA, 0x03, 0xF0, 0xE2, 0xAA, 0x03, 0xF0,
         0x15, 0,    2,  0xBD, 0x00,
         0x39, 0,    3,  0xB6, 0x8D, 0x8D,
-        0x15, 0,    2,  0xCC, 0x09,
+        0x15, 0,    2,  0xCC, 0x08,
         0x39, 0,    3,  0xC6, 0xFF, 0xF9,
         0x39, 0,   55,  0xE0, 0x00, 0x12, 0x1f, 0x1a, 0x40, 0x4a, 0x59, 0x55, 0x5e, 0x67, 0x6f, 0x75, 0x7a, 0x82, 0x8b, 0x90, 0x95, 0x9f, 0xa3, 0xad, 0xa2, 0xb2, 0xB6, 0x5e, 0x5a, 0x65, 0x77, 0x00, 0x12, 0x1f, 0x1a, 0x40, 0x4a, 0x59, 0x55, 0x5e, 0x67, 0x6f, 0x75, 0x7a, 0x82, 0x8b, 0x90, 0x95, 0x9f, 0xa3, 0xad, 0xa2, 0xb2, 0xB6, 0x5e, 0x5a, 0x65, 0x77,
         0x15, 100,  1,  0x11,
@@ -76,34 +69,36 @@ static void hx8399_v2_init(k_u8 test_mode_en)
 static void hx8399_power_reset(k_s32 on)
 {
     k_u8 rst_gpio;
+
     if(0 > (rst_gpio = CONFIG_MPP_DSI_LCD_RESET_PIN)) {
         return;
     }
 
     kd_pin_mode(rst_gpio, GPIO_DM_OUTPUT);
-    
-    if (on)
-        kd_pin_write(rst_gpio, GPIO_PV_HIGH); // GPIO_PV_LOW  GPIO_PV_HIGH
-    else
-        kd_pin_write(rst_gpio, GPIO_PV_LOW); // GPIO_PV_LOW  GPIO_PV_HIGH
 
+    if (on) {
+        kd_pin_write(rst_gpio, GPIO_PV_HIGH);
+    } else {
+        kd_pin_write(rst_gpio, GPIO_PV_LOW);
+    }
 }
 
 static void hx8399_set_backlight(k_s32 on)
 {
     k_u8 backlight_gpio;
+
     if(0 > (backlight_gpio = CONFIG_MPP_DSI_LCD_BACKLIGHT_PIN)) {
         return;
     }
 
     kd_pin_mode(backlight_gpio, GPIO_DM_OUTPUT);
-    if (on)
+
+    if (on) {
         kd_pin_write(backlight_gpio, GPIO_PV_HIGH);
-    else
+    } else {
         kd_pin_write(backlight_gpio, GPIO_PV_LOW);
-
+    }
 }
-
 
 static k_s32 hx8399_power_on(void* ctx, k_s32 on)
 {
@@ -113,23 +108,22 @@ static k_s32 hx8399_power_on(void* ctx, k_s32 on)
     if (on) {
         // rst vo;
         k230_display_rst();
+
         // rst hx8399 
         hx8399_power_reset(1);
-        rt_thread_mdelay(g_blacklight_delay_ms);
+        rt_thread_mdelay(10);
         hx8399_power_reset(0);
-        rt_thread_mdelay(g_blacklight_delay_ms);
+        rt_thread_mdelay(50);
         hx8399_power_reset(1);
 
-        g_blacklight_delay_ms = DELAY_MS_BACKLIGHT_DEFAULT;
         //enable backlight
         hx8399_set_backlight(1);
     } else {
         hx8399_set_backlight(0);
     }
-    
+
     return ret;
 }
-
 
 static k_s32 hx8399_set_phy_freq(k_connectori_phy_attr *phy_attr)
 {
@@ -147,7 +141,6 @@ static k_s32 hx8399_set_phy_freq(k_connectori_phy_attr *phy_attr)
     return 0;
 }
 
-
 static k_s32 hx8399_dsi_resolution_init(k_connector_info *info)
 {   
     k_vo_dsi_attr attr;
@@ -162,19 +155,16 @@ static k_s32 hx8399_dsi_resolution_init(k_connector_info *info)
     memcpy(&attr.resolution, &resolution, sizeof(k_vo_display_resolution));
     connector_set_dsi_attr(&attr);
 
-    if(info->screen_test_mode)
-        hx8399_v2_init(1);
-    else
-        hx8399_v2_init(0);
+    hx8399_v2_init(info->screen_test_mode);
 
     connector_set_dsi_enable(1);
 
-    if(info->dsi_test_mode == 1)
+    if(info->dsi_test_mode) {
         connector_set_dsi_test_mode();
+    }
 
     return 0;
 }
-
 
 static k_s32 hx8399_vo_resolution_init(k_vo_display_resolution *resolution, k_u32 bg_color, k_u32 intr_line)
 {
@@ -194,15 +184,15 @@ static k_s32 hx8399_vo_resolution_init(k_vo_display_resolution *resolution, k_u3
 
     return 0;
 }
-   
 
 k_s32 hx8399_init(void *ctx, k_connector_info *info)
 {
     k_s32 ret = 0;
     struct connector_driver_dev* dev = ctx;
 
-    if(info->pixclk_div != 0)
+    if(info->pixclk_div != 0) {
         connector_set_pixclk(info->pixclk_div);
+    }
 
     ret |= connector_set_cmd_buff_num(info->buff_num);
     ret |= hx8399_set_phy_freq(&info->phy_attr);
@@ -219,7 +209,6 @@ static k_s32 hx8399_get_chip_id(void* ctx, k_u32* chip_id)
     return ret;
 }
 
-
 static k_s32 hx8399_conn_check(void* ctx, k_s32* conn)
 {
     k_s32 ret = 0;
@@ -229,29 +218,13 @@ static k_s32 hx8399_conn_check(void* ctx, k_s32* conn)
     return ret;
 }
 
-
 static k_s32 hx8399_set_mirror(void* ctx, k_connector_mirror *mirror)
 {
-    k_connector_mirror hx8399_mirror;
+    (void)ctx;
+    (void)mirror;
 
-    hx8399_mirror = *mirror;
-
-    switch(hx8399_mirror)
-    {
-        case K_CONNECTOR_MIRROR_HOR:
-            break;
-        case K_CONNECTOR_MIRROR_VER: 
-            hx8399_y_mirror = 0x04;
-            break;
-        case K_CONNECTOR_MIRROR_BOTH: 
-            break;
-        default :
-            rt_kprintf("hx8399_mirror(%d) is not support \n", hx8399_mirror);
-            break;
-    }
     return 0;
 }
-
 
 struct connector_driver_dev hx8399_connector_drv = {
     .connector_name = "hx8399",
