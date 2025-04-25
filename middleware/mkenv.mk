81a03bffad1ec29b3cd86a66a9578af709a6585e
kendryte747 <kendryte747@gmail.com>
2025-04-25 16:06:25 +0800

    Fix build not copy resources.

--- middleware/mkenv.mk
+++ middleware/mkenv.mk
@@ -1,12 +1,12 @@
 ifneq ($(MKENV_INCLUDED),1)
 export SDK_SRC_ROOT_DIR := $(realpath $(dir $(realpath $(lastword $(MAKEFILE_LIST))))/../../../../)
+endif
 
 include $(SDK_SRC_ROOT_DIR)/tools/mkenv.mk
 
 export MPP_SRC_DIR := $(SDK_RTSMART_SRC_DIR)/mpp/
 export RTSMART_SRC_DIR := $(SDK_RTSMART_SRC_DIR)/rtsmart/
 export MPP_BUILD_DIR := $(SDK_RTSMART_BUILD_DIR)/mpp/
-endif
 
 export MPP_MIDDLEWARE_LIB_INSTALL_PATH := $(MPP_SRC_DIR)/middleware/lib/
 $(shell if [ ! -e $(MPP_MIDDLEWARE_LIB_INSTALL_PATH) ];then mkdir -p $(MPP_MIDDLEWARE_LIB_INSTALL_PATH); fi)
